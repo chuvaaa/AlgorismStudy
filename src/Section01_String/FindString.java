2fcc699d7bf5223ab919275a1f8dc05fe9bdabb4
김민수 <minsu@sk.com>
2023-02-13 21:32:50 +0900

    chore : 주석 수정

--- src/Section01_String/FindString.java
+++ src/Section01_String/FindString.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * @author a1101466 on 2023/02/13
  * @project AlgorismStudy
- * @description SECTION01 문자 찾기.
+ * @description SECTION01 1번 문제 문자 찾기.
  */
 public class FindString {
 
